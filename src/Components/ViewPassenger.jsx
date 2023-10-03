ae3389ee9f853e42d5ea74ba82074bacd2df5839
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-03 22:43:21 +0530

    Project Completed and Errors Rectified

--- src/Components/ViewPassenger.jsx
+++ src/Components/ViewPassenger.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Header from './Header'
 
 const ViewPassenger = () => {
     const [passData, setPassData] = useState(
