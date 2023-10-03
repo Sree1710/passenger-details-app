cabf9d28aab7f38f6132cad1b2cd69bf87863c27
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-03 22:32:26 +0530

    Routing Implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ViewPassenger from './Components/ViewPassenger';
+import AddPassenger from './Components/AddPassenger';
+import SearchPassenger from './Components/SearchPassenger';
 
 
 function App() {
   return (
     <div>
-     <ViewPassenger/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<AddPassenger/>} />
+      <Route path='/sp' exact element={<SearchPassenger/>}/>
+      <Route path='/vp' exact element={<ViewPassenger/>} />
+    </Routes>
+    </BrowserRouter> 
     </div>
   );
 }
