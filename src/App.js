e12902810790bd035db17e20dd9e647049a2c0b6
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-03 21:17:00 +0530

    new branch view-passenger created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import AddPassenger from './Components/AddPassenger';
-import SearchPassenger from './Components/SearchPassenger';
+
 
 function App() {
   return (
     <div>
-      <SearchPassenger/>
+
     </div>
   );
 }
