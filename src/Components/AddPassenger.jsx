273b4ef0fcd0db04b29c6e67f6bfcdf8ee3cde9b
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-03 17:18:01 +0530

    AddPassenger component completed

--- src/Components/AddPassenger.jsx
+++ src/Components/AddPassenger.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddPassenger = () => {
+    const [inputField,setInputField]=useState(
+        {name:"",trips:""}
+    )
+
+    const inputHandler=(event)=>{
+        setInputField({...inputField,[event.target.name]:event.target.value})
+    }
+
+    const readValue=()=>{
+        console.log(inputField)
+    }
   return (
-    <div>AddPassenger</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <h1>Add Passenger</h1>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="name" value={inputField.name} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">No: Of Trips</label>
+                            <input onChange={inputHandler} type="text" className="form-control" name="trips" value={inputField.trips} />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button onClick={readValue} className="btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
